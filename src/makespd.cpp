401cef83c2992fab63215351c24caa3489a8c067
Simon Brewer <brewersi@gmail.com>
2023-09-06 20:46:43 -0600

    Initial commit with basic working version of spd

---
+++ src/makespd.cpp
@@ -0,0 +1,37 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+Rcpp::NumericVector  make_spd(Rcpp::List input_val, 
+                             int start_date,
+                             int end_date){
+  
+  if(end_date <= start_date) { 
+    Rcpp::stop("end_date should be larger than start_date");
+  }
+  // Create vector to store sum of probability
+  Rcpp::NumericVector prsum (end_date - start_date);
+  
+  
+  // Get size of lists (no of dates)
+  unsigned int n = input_val.length();
+  unsigned int elems = 0;
+  
+  // loop over dates
+  for(unsigned int i = 0; i < n; i++) {
+    Rcpp::DataFrame df(input_val[i]);
+    Rcpp::IntegerVector calbp = df["calBP"];
+    Rcpp::NumericVector prdens = df["PrDens"];
+    //Rcpp::DataFrame dates = input_val[i];
+    elems = calbp.length();
+    for(unsigned int j = 0; j < elems; j++) {
+      if (calbp(j) > start_date && calbp(j) < end_date) {
+        prsum(calbp(j)) += prdens(j);
+      }
+    }
+    
+  }
+  
+  return prsum;
+}
+
