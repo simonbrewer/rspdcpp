8f002201aaee7b671bce05266543be3b66ca5898
Simon Brewer <brewersi@gmail.com>
2023-09-06 21:57:12 -0600

    Added simple benchmark

--- R/01_spd_tests.R
+++ R/01_spd_tests.R
@@ -2,11 +2,20 @@ library(Rcpp)
 sourceCpp("./src/makespd.cpp")
 
 load("./data/uncalDates.RData")
+load("./data/spdExample.RData")
 
 test_dates = list(grid = list(uncalsample.norm$grids[[1]],
                               uncalsample.norm$grids[[2]])
 )
 
 make_spd(test_dates$grid, 0, 4000)
-make_spd(uncalsample.norm$grids, 0, 4000) -> x
+
+## Results comparison
+spd_test <- make_spd(uncalsample.norm$grids, 0, 4000)
+
+plot(spd.uncalsample.norm$grid$calBP, spd.uncalsample.norm$grid$PrDens, type = 'l')
+lines(spd_test$calbp, spd_test$d, col = 2)
+
+## RCARBON comparison below here
+library(rcarbon)
 
