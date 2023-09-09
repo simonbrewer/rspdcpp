c9dcddd51d6ced34df751d8f24cbd837e7a2d8b2
Simon Brewer <brewersi@gmail.com>
2023-09-09 15:43:49 -0600

    Added time benchmarks and plots

--- R/02_calibration_tests.R
+++ R/02_calibration_tests.R
@@ -1,12 +1,31 @@
 ## Comparison of rcarbon and rcpp calibration
-
+library(Rcpp)
 sourceCpp("./src/calibrate.cpp")
 
 load("data/dates.RData")
-
+system.time(dates_rcarbon <- 
+              rcarbon::calibrate(uncal.samples, errors, calCurves = 'intcal20', 
+                                 ids = ids, normalised = TRUE, verbose = FALSE))
 
 ## RCPP - need to load CalCurve
 cc <- read.table("./data/intcal20.14c", header = FALSE, sep = ',')[, 1:3]
 colnames(cc) <- c("CALBP","C14BP","Error")
 cc <- cc[order(cc$CALBP), ]
-dates_rcpp <- calibrate(age, error, cc, 0, 10000)
+## Full date range
+system.time(dates_rcpp <- calibrate(uncal.samples, errors, cc, 0, 55000,
+                                    normalize = TRUE)) 
+plot(dates_rcarbon$grids[[1]])
+lines(dates_rcpp[[1]], col = 2)
+## Differeces
+rev(dates_rcpp[[1]]$prdens) - dates_rcarbon$grids[[1]]$PrDens
+
+## Subset range 0-10000
+system.time(dates_rcpp <- calibrate(uncal.samples, errors, cc, 0, 10000,
+                                    normalize = TRUE)) 
+plot(dates_rcarbon$grids[[1]])
+lines(dates_rcpp[[1]], col = 2)
+## Differeces
+rev(dates_rcpp[[1]]$prdens) - dates_rcarbon$grids[[1]]$PrDens
+
+
+
