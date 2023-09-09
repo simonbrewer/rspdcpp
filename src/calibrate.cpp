bb17f55ce24829c1792aea57b085923070af29ae
Simon Brewer <brewersi@gmail.com>
2023-09-09 09:35:22 -0600

    Functioning dnorm for single date (errors are wrong)

--- src/calibrate.cpp
+++ src/calibrate.cpp
@@ -2,6 +2,16 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
+Rcpp::NumericVector mydnorm( Rcpp::NumericVector x, 
+                             Rcpp::NumericVector means, 
+                             Rcpp::NumericVector sds){
+  int n = x.size() ;
+  Rcpp::NumericVector res(n) ;
+  for( int i=0; i<n; i++) res[i] = R::dnorm( x[i], means[i], sds[i], false ) ;
+  return res ;
+}
+
+// [[Rcpp::export]]
 double cpplinterp_(Rcpp::NumericVector x,
                    Rcpp::NumericVector y, 
                    double xout, const int maxgap=99999, const bool locf=false){
@@ -84,8 +94,8 @@ Rcpp::NumericVector cpplinterp(Rcpp::NumericVector x,
 
 // [[Rcpp::export]]
 double approx_test(Rcpp::DataFrame calcurve, 
-                        int start_date,
-                        int end_date){
+                   int start_date,
+                   int end_date){
   
   Rcpp::NumericVector calbp = calcurve["CALBP"];
   Rcpp::NumericVector c14bp = calcurve["C14BP"];
@@ -98,23 +108,36 @@ double approx_test(Rcpp::DataFrame calcurve,
 }
 
 // [[Rcpp::export]]
-NumericVector calibrate(Rcpp::DataFrame calcurve, 
+NumericVector calibrate(Rcpp::NumericVector ages, 
+                        Rcpp::NumericVector error, 
+                        Rcpp::DataFrame calcurve, 
                         int start_date,
                         int end_date){
+  
+  // Step 1: get yearly conversion series
   Rcpp::NumericVector calbp = calcurve["CALBP"];
   Rcpp::NumericVector c14bp = calcurve["C14BP"];
   Rcpp::NumericVector tau1 = calcurve["Error"];
   
   // Interpolation grid
   IntegerVector tmprange = Rcpp::seq(start_date, end_date);
-  NumericVector agerange = as<NumericVector>(tmprange);
-  NumericVector mu = cpplinterp(calbp, c14bp, agerange, 99999, false);
-  NumericVector tau2 = cpplinterp(calbp, tau1, agerange, 99999, false);
-  tau2 = tau2 * tau2;
-  // Rcpp::NumericVector mu = Rcpp::clone(agerange);
-  // for(int i=0;i<agerange.size();i++){
-  //   mu[i]=cpplinterp_(calbp,c14bp,agerange[i]);
+  NumericVector agegrid = as<NumericVector>(tmprange);
+  int ngrid = agegrid.length();
+  NumericVector mu = cpplinterp(calbp, c14bp, agegrid, 99999, false);
+  NumericVector tau2 = cpplinterp(calbp, tau1, agegrid, 99999, false);
+  tau2 = pow(tau2, 2);
+  
+  // Convert dates
+  NumericVector tau = pow(error, 2.0) + tau2;
+  tau = pow(tau, 0.5);
+  double x1 = ages[0];
+  double x2 = mu[0];
+  double x3 = tau[0];
+  //double dens = dnorm(x1, mean = x2, sd = x3);
+  NumericVector dens = mydnorm(agegrid, mu, tau);
+  // loop over dates
+  // for(unsigned int j = 0; j < ngrid; j++) {
   // }
-  return tau2;
+  return dens;
 }
 
